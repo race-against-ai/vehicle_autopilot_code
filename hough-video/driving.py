d969d30c6104455b2c2d4dd0b6b64e932ff59dc5
Marvin-VW <82050751+Marvin-VW@users.noreply.github.com>
2024-02-29 15:07:19 +0100

    run algorithm locally with video

---
+++ hough-video/driving.py
@@ -0,0 +1,20 @@
+#rfom piracer.vehicles import PiRacerPro
+
+#from adafruit_ssd1306 import SSD1306_I2C
+#import board
+#import busio
+
+battery_percent_backup = 100
+status = 2 #status = 1 charging, status = 2 no charging
+
+
+class Functions_Driving:
+    def __init__(self):
+        test = 0
+        #self.piracer = PiRacerPro()
+
+    #steering left
+    def left_steering(self, percent):
+        test = 2
+    
+    
