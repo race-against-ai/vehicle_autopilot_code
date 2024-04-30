121f98275ebd40af6e32f6ef6d2494ef1b93c18d
Marvin <82050751+Marvin-VW@users.noreply.github.com>
2024-04-30 10:50:37 +0200

    ui for stream

---
+++ ui/stream-process/calibra.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+
+data = np.load('calib_data/MultiMatrix.npz')
+camMatrix = data["camMatrix"]
+distCof = data["distCoef"]
+rVector = data["rVector"]
+tVector = data["tVector"]
+
+def calib(frame):
+
+    #undistore
+    corrected_image = cv2.undistort(frame, camMatrix, distCof, None, camMatrix)
+
+    return corrected_image
